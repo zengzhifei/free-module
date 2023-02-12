d7d42ba414b86da42172f29a8eda36d1cf9a2a80
zengzhifei <zengzhifei@outlook.com>
2023-02-12 22:42:57 +0800

    add event bus

---
+++ free-common-module/src/main/java/com/stoicfree/free/common/module/support/eventbus/EventBusMessage.java
@@ -0,0 +1,19 @@
+package com.stoicfree.free.common.module.support.eventbus;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author zengzhifei
+ * @date 2023/2/2 12:20
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class EventBusMessage<T> {
+    private Enum<?> event;
+    private T data;
+}
