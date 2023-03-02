58037c3b83f08c79d3a22b9c30e432bdd51d7bc5
zengzhifei <zengzhifei@outlook.com>
2023-03-02 16:19:11 +0800

    update new Gson

--- free-module-core/src/main/java/com/stoicfree/free/module/core/common/gson/GsonUtil.java
+++ free-module-core/src/main/java/com/stoicfree/free/module/core/common/gson/GsonUtil.java
@@ -28,6 +28,7 @@ import lombok.extern.slf4j.Slf4j;
  */
 @Slf4j
 public class GsonUtil {
+    public static final Gson JSON = new Gson();
     private static final Gson GSON;
 
     private GsonUtil() {
