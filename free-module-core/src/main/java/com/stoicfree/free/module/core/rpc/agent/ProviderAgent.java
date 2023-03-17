4f35104e4c5616164fc8b0b1ffdb3e15d5294455
zengzhifei <zengzhifei@outlook.com>
2023-03-17 19:01:30 +0800

    add bean contanir

--- free-module-core/src/main/java/com/stoicfree/free/module/core/rpc/agent/ProviderAgent.java
+++ free-module-core/src/main/java/com/stoicfree/free/module/core/rpc/agent/ProviderAgent.java
@@ -2,37 +2,42 @@ package com.stoicfree.free.module.core.rpc.agent;
 
 import java.lang.reflect.Method;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.commons.lang.StringUtils;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.annotation.AnnotationConfigurationException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.stoicfree.free.module.core.common.support.AnnotatedBeanContainer;
+import com.stoicfree.free.module.core.common.support.TwoTuple;
+import com.stoicfree.free.module.core.rpc.annotation.ProviderClass;
+import com.stoicfree.free.module.core.rpc.annotation.ProviderMethod;
 import com.stoicfree.free.module.core.rpc.config.ProviderProperties;
 import com.stoicfree.free.module.core.rpc.domain.ProxyRequest;
 import com.stoicfree.free.module.core.rpc.domain.ProxyResponse;
 import com.stoicfree.free.module.core.rpc.exception.ProviderException;
 import com.stoicfree.free.module.core.rpc.safe.SecureKeeper;
 
-import javafx.util.Pair;
-
 /**
  * @author zengzhifei
  * @date 2023/2/26 00:15
  */
 @RestController
-public class ProviderAgent {
+public class ProviderAgent extends AnnotatedBeanContainer {
+    private final Map<String, TwoTuple<Object, Method>> providers = new HashMap<>();
     private final ProviderProperties providerProperties;
 
-    @Autowired
-    private ProviderContainer providerContainer;
-
     public ProviderAgent(ProviderProperties providerProperties) {
+        super(ProviderClass.class, ProviderMethod.class);
+
         this.providerProperties = providerProperties;
     }
 
@@ -44,19 +49,41 @@ public class ProviderAgent {
             String providerId = proxyRequest.getProviderId();
             Object[] params = proxyRequest.getParams();
 
-            Pair<Object, Method> provider = providerContainer.getProvider(providerId);
+            TwoTuple<Object, Method> provider = providers.get(providerId);
             if (provider == null) {
                 throw new ProviderException(String.format("%s is not register", providerId));
             }
 
-            Object bean = provider.getKey();
-            Method method = provider.getValue();
+            Object bean = provider.getFirst();
+            Method method = provider.getSecond();
             method.setAccessible(true);
 
             Object response = method.invoke(bean, params);
             return ProxyResponse.success(response);
         } catch (ProviderException e) {
             return ProxyResponse.fail(e.getMessage());
+        }
+    }
+
+    @Override
+    protected void afterInitAnnotatedBeanContainer() {
+        Map<Object, List<Method>> beanMethods = getAnnotatedBeanMethods();
+        for (Map.Entry<Object, List<Method>> entry : beanMethods.entrySet()) {
+            Object bean = entry.getKey();
+            List<Method> methods = entry.getValue();
+            for (Method method : methods) {
+                ProviderMethod providerMethod = method.getDeclaredAnnotation(ProviderMethod.class);
+                String id = providerMethod.id();
+                if (StringUtils.isBlank(id)) {
+                    throw new AnnotationConfigurationException(String.format("%s %s ProviderMethod id not be blank",
+                            bean.getClass().getTypeName(), method.getName()));
+                }
+                if (providers.containsKey(id)) {
+                    throw new AnnotationConfigurationException(String.format("%s %s ProviderMethod id not be repeat",
+                            bean.getClass().getTypeName(), method.getName()));
+                }
+                providers.put(id, TwoTuple.of(bean, method));
+            }
         }
     }
 
--- free-module-starter/src/main/java/com/stoicfree/free/module/autoconfigure/RpcModuleAutoConfiguration.java
+++ free-module-starter/src/main/java/com/stoicfree/free/module/autoconfigure/RpcModuleAutoConfiguration.java
@@ -1,13 +1,11 @@
 package com.stoicfree.free.module.autoconfigure;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.stoicfree.free.module.core.rpc.agent.ProviderAgent;
-import com.stoicfree.free.module.core.rpc.agent.ProviderContainer;
 import com.stoicfree.free.module.core.rpc.config.RpcProperties;
 
 /**
@@ -21,12 +19,6 @@ public class RpcModuleAutoConfiguration {
     private RpcProperties rpcProperties;
 
     @Bean
-    public ProviderContainer providerContainer() {
-        return new ProviderContainer();
-    }
-
-    @Bean
-    @ConditionalOnBean(ProviderContainer.class)
     public ProviderAgent providerAgent() {
         return new ProviderAgent(rpcProperties.getProvider());
     }
